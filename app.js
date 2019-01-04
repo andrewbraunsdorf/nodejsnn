e2d8f015695b4350c348a091a1c75108eed6d338
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-04 02:00:05 +0000

    Module Patterns

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var counter = require("./count");
+var stuff = require("./stuff");
 
 
-console.log(counter(["shawn", "crystal", "ryan"]));
+console.log(stuff.counter(["shawn", "crystal", "ryan"]));
+console.log(stuff.adder(5,9));
+console.log(stuff.adder(stuff.pi, 5));
