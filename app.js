010cf27921b2e37d9b62da1360a3e9514f58418a
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-05 15:37:20 +0000

    node event emitters

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-var stuff = require("./stuff");
+var events = require("events");
+var util = require("util");
 
+var Person = function(name){
+    this.name = name;
+};
 
-console.log(stuff.counter(["shawn", "crystal", "ryan"]));
-console.log(stuff.adder(5,9));
-console.log(stuff.adder(stuff.pi, 5));
+util.inherits(Person, events.eventEmitter);
+
+var james = new Person("james");
+var mary = new Person("mary");
+var ryu = new Person("ryu");
+var people = [james, mary, ryu];
+
+people.forEach(function(person){
+   person.on("speak" , function(mssg){
+       console.log(person.name + "said" + mssg);
+   }); 
+});
+
+james.emit("speak", "hey dudes");
+ryu.emit("speak", "I want a curry");
+// var myEmitter = new events.EventEmitter();
+
+// myEmitter.on("someEvent", function(mssg){
+//     console.log(mssg);
+    
+// });
+
+// myEmitter.emit("someEvent", "the event was emitted");
