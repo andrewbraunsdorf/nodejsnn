3e2d9149aa98e8bacc013d1ee4b3ddf01ddc1ba8
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-10 02:01:57 +0000

    Readable Streams

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 var http = require("http");
+var fs = require("fs");
 
-var server = http.createServer(function(req, res){
-	console.log("request was made: " + req.url);
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("Hey ninjas");
+var myReadStream = fs.createReadStream(__dirname + "/clientsandservers.txt", "utf8");
+
+myReadStream.on("data", function(chunk){
+	console.log("new chunk received:");
+	console.log(chunk);
 });
 
-server.listen(8080, "0.0.0.0");
-console.log("yo dawgs, now listening to port 8080");
+
+
+
+
+
 
 // var http = require("http");
 
 // var server = http.createServer(function(req, res){
+// 	console.log("request was made: " + req.url);
 // 	res.writeHead(200, {"Content-Type": "text/plain"});
 // 	res.end("Hey ninjas");
 // });
 
-// server.listen(8080, "35.184.86.113");
+// server.listen(8080, "0.0.0.0");
 // console.log("yo dawgs, now listening to port 8080");
+
