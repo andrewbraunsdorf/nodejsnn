65f971574e6e4b00a32e720dfcd7f89fb787fbf1
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-10 03:33:28 +0000

    Pipes

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
 var http = require("http");
 var fs = require("fs");
 
-var myReadStream = fs.createReadStream(__dirname + "/clientsandservers.txt", "utf8");
-var myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt");
-
-myReadStream.on("data", function(chunk){
-	console.log("new chunk received:");
-	myWriteStream.write(chunk,)
-	// console.log(chunk);
+var server = http.createServer(function(req, res) {
+	console.log("request was made: " + req.url);
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	var myReadStream = fs.createReadStream(__dirname + "/clientsandservers.txt", "utf8");
+	myReadStream.pipe(res);
 });
 
 
-
-
-
-
-
-// var http = require("http");
-
-// var server = http.createServer(function(req, res){
-// 	console.log("request was made: " + req.url);
-// 	res.writeHead(200, {"Content-Type": "text/plain"});
-// 	res.end("Hey ninjas");
-// });
-
-// server.listen(8080, "0.0.0.0");
-// console.log("yo dawgs, now listening to port 8080");
-
+server.listen(8080, "0.0.0.0");
+console.log("yo dawgs, now listening to port 8080");
