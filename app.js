a07ddc392d79f2a6eae60c1f8088a87f2aade4ea
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-10 23:28:46 +0000

    Serving JSON Data

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var fs = require("fs");
 
 var server = http.createServer(function(req, res) {
 	console.log("request was made: " + req.url);
-	res.writeHead(200, { "Content-Type": "text/html" });
-	var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
-	myReadStream.pipe(res);
+	res.writeHead(200, { "Content-Type": "application/json" });
+	var myObj = {
+		name: "ryu",
+		job: "Ninja",
+		age: 29
+	};
+	res.end(JSON.stringify(myObj));
 });
 
 
 server.listen(8080, "0.0.0.0");
 console.log("yo dawgs, now listening to port 8080");
+
+	// var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
+	// myReadStream.pipe(res);
