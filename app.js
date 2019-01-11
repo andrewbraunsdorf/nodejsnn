1740bb2bd99a3e24573cc4c3f6c074dbf8bfa4a1
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-11 00:18:31 +0000

    Basic Routing

--- app.js
+++ app.js
@@ -3,18 +3,42 @@ var fs = require("fs");
 
 var server = http.createServer(function(req, res) {
 	console.log("request was made: " + req.url);
-	res.writeHead(200, { "Content-Type": "application/json" });
-	var myObj = {
-		name: "ryu",
-		job: "Ninja",
-		age: 29
-	};
-	res.end(JSON.stringify(myObj));
+	if(req.url === "/home" || req.url === "/"){
+		res.writeHead(200, {"Content-Type": "text/html"});
+		fs.createReadStream(__dirname + "/index.html").pipe(res);
+	} else if(req.url === "/contact"){
+		res.writeHead(200, {"Content-Type": "text/html"});
+		fs.createReadStream(__dirname + "/contact.html").pipe(res);	
+	} else if (req.url === "/api/ninjas"){
+		var ninjas =[{name: "ryu", age: 29}, {name: "yoshi", age: 32}];
+		res.writeHead(200, {"Content-Type": "application/json"});
+		res.end(JSON.stringify(ninjas));
+		} else{
+		res.writeHead(404, {"Content-Type": "text/html"});
+		fs.createReadStream(__dirname + "/404.html").pipe(res);
+		}
 });
 
 
 server.listen(8080, "0.0.0.0");
 console.log("yo dawgs, now listening to port 8080");
 
-	// var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
-	// myReadStream.pipe(res);
+
+// var http = require("http");
+// var fs = require("fs");
+
+// var server = http.createServer(function(req, res) {
+// 	console.log("request was made: " + req.url);
+// 	res.writeHead(200, { "Content-Type": "application/json" });
+// 	var myObj = {
+// 		name: "ryu",
+// 		job: "Ninja",
+// 		age: 29
+// 	};
+// 	res.end(JSON.stringify(myObj));
+// });
+
+
+// server.listen(8080, "0.0.0.0");
+// console.log("yo dawgs, now listening to port 8080");
+
