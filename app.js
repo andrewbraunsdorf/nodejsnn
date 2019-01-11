7244b7be71a81a5d202928df9f3aa565c07cc97c
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-11 01:07:16 +0000

    Express Route Params

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var app = express();
 app.get("/", function(req, res){
 	res.send("This is the homepage");	
 });
+
 app.get("/contact", function(req, res){
-	res.send("This is the contact page");	
+	res.send("This is the contact page");
+});
+
+app.get("/profile/:name", function(req, res){
+	res.send("You requested to see a profile with the name of " + req.params.name);
 });
 
 app.listen(8080);
