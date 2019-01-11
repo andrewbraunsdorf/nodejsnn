b2e993791f31f8822aa2e593cd53db01318d8f3e
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-11 00:31:05 +0000

    saved page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 
 var server = http.createServer(function(req, res) {
 	console.log("request was made: " + req.url);
-	if(req.url === "/home" || req.url === "/"){
+	if(req.url === "/home" || req.url === "/"){ 
 		res.writeHead(200, {"Content-Type": "text/html"});
 		fs.createReadStream(__dirname + "/index.html").pipe(res);
 	} else if(req.url === "/contact"){
