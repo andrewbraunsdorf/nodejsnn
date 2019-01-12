6eca8aac507007bd58f8acdf786b852148d1f216
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-12 18:33:27 +0000

    Partial Templates

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-	res.sendFile(__dirname + "/index.html");	
+	res.render("index");	
 });
 
 app.get("/contact", function(req, res){
-	res.sendFile(__dirname + "/contact.html");
+	res.render("contact");
 });
 
-app.get("/404", function(req, res){
-	res.sendFile(__dirname + "/404.html");
-});
 app.get("/profile/:name", function(req, res){
 	var data = {age: 29, job: "ninja", hobbies: ["eating", "fighting", "fishing"]};
 	res.render("profile", {person: req.params.name, data: data});
