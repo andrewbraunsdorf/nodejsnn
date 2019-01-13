ae668a908bed78032bda827b42b46cba18207f62
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-01-13 18:52:38 +0000

    Finished todo app, and linked it all to mongodb via mLab.

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -11,30 +11,45 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo =mongoose.model("Todo", todoSchema);
-var itemOne = Todo({item: "get flowers"}).save(function(err){
-	if (err) throw err;
-	console.log("item saved");
-});
 
-var data = [{item: "get milk"}, {item: "walk dog"}, {item: "kick some coding ass"}];
+
+//var data = [{item: "get milk"}, {item: "walk dog"}, {item: "kick some coding ass"}];
 var urlencodedParser =bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
 
 app.get("/todo", function(req, res){
-	res.render("todo", {todos: data});
+	//get data from mongodb and pass it to view
+	Todo.find({}, function(err, data){
+		if (err) throw err;
+		res.render("todo", {todos: data});
+	});
+	
 });
 
 app.post("/todo", urlencodedParser, function(req, res){
-	data.push(req.body);
-	res.json(data);
+	//get data from the view and add it to mongodb
+	var newTodo = Todo(req.body).save(function(err, data){
+		if (err) throw err;
+		res.json(data);
+	});
 });
 
 app.delete("/todo/:item", function(req, res){
-	data = data.filter(function(todo){
-		return todo.item.replace(/ /g, "-") !== req.params.item;
+	//delete the requested item from mongodb
+	//instead of .remove used ((node:7941) DeprecationWarning: collection.remove is deprecated. Use deleteOne, deleteMany, or bulkWrite instead.)
+	Todo.find({item: req.params.item.replace(/\-/g, " ")}).deleteOne(function(err, data){
+		if (err) throw err;
+		res.json(data);
 	});
-	res.json(data);
 });
 
 };
+
+
+
+// var Todo =mongoose.model("Todo", todoSchema);
+// // var itemOne = Todo({item: "buy flowers"}).save(function(err){
+// // 	if (err) throw err;
+// // 	console.log("item saved");
+// // });
